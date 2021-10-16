8fc17026f2d91d82ceaba45aaad46226755b0c68
손채건 <thscorjs1002@naver.com>
2021-10-16 23:35:10 +0900

    Create BallotV4.sol

---
+++ BallotV4.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.1;
+
+contract BallotV4 {
+    
+    struct Voter {
+        uint weight;
+        bool voted;
+        uint vote;
+    }
+    
+    struct Proposal {
+        uint voteCount;
+    }
+    
+    address chairperson;
+    mapping(address => Voter) voters;
+    Proposal[] proposals;
+    
+    enum Phase { Init, Regs, Vote, Done }
+    Phase public state = Phase.Init;
+    
+    modifier validPhase(Phase reqPhase) {
+        require(state == reqPhase);
+        _;
+    }
+    
+    modifier onlyChair() {
+        require(msg.sender == chairperson);
+        _;
+    }
+    
+    constructor (uint numProposals) {
+        chairperson = msg.sender;
+        voters[chairperson].weight = 2;
+        for (uint prop = 0; prop < numProposals; prop++) {
+            proposals.push(Proposal(0));
+        }
+        state = Phase.Regs;
+    }
+    
+    function changeState(Phase x) public onlyChair {
+        if (x < state) revert();
+        state = x;
+    }
+    
+    function register(address voter) public validPhase(Phase.Regs) onlyChair {
+        require(!voters[voter].voted)) revert();
+        voters[voter].weight = 1;
+        voters[voter].voted = false;
+    }
+    
+    function vote(uint toProposal) public validPhase(Phase.Vote) {
+        Voter memory sender = voters[msg.sender];
+        require(!sender.voted);
+        require(toProposal < proposals.length);
+        sender.voted = true;
+        sender.vote = toProposal;
+        proposals[toProposal].voteCount += sender.weight;
+    }
+    
+    function reqWinner() public validPhase(Phase.Done) view returns (uint winningProposal) {
+        uint winningVoteCount = 0;
+        for (uint prop = 0; prop < proposals.length; prop++) {
+            if (proposals[prop].voteCount > winningVoteCount) {
+                winningVoteCount = proposals[prop].voteCount;
+                winningProposal = prop;
+            }
+        }
+        assert(winningVoteCount >= 3);
+    }
+}
