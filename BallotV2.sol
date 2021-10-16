cd170af40416d8052fc91898341d16d43c7aac85
손채건 <thscorjs1002@naver.com>
2021-10-16 23:12:07 +0900

    Create BallotV2.sol

---
+++ BallotV2.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.1;
+
+contract BallotV2 {
+    
+    struct Voter {
+        uint weight;
+        bool voted;
+        uint vote;
+    }
+    
+    struct Proposal {
+        uint voteCount;
+    }
+    
+    address chairperson;
+    mapping(address => Voter) voters;
+    Proposal[] proposals;
+    
+    enum Phase { Init, Regs, Vote, Done }
+    Phase public state = Phase.Init;
+    
+    constructor (uint numProposals) public {
+        chairperson = msg.sender;
+        voters[chairperson].weight = 2;
+        for (uint prop = 0; prop < numProposals; prop++) {
+            proposals.push(Proposal(0));
+        }
+    }
+    
+    function changeState(Phase x) public {
+        if (msg.sender != chairperson) revert();
+        if (x < state) revert();
+        state = x;
+    }
+}
